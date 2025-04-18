6fc995ecd8b5f10c5be9f3c6d561f8823638396d
Mohammad Shabeer <moshdev2213@gmail.com>
2025-04-18 06:29:35 +0530

    fix:appFE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import AntdHeader from './components/header/AntdHeader';
 
 function App() {
     const userRole = localStorage.getItem('userRole');
-
     return (
         <>
             <BrowserRouter>

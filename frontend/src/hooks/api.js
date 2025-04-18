bd5d8319356432034b04f7e3efd0d5d1381a18ee
Mohammad Shabeer <moshdev2213@gmail.com>
2025-04-18 05:55:07 +0530

    fix:feBaseURL

--- frontend/src/hooks/api.js
+++ frontend/src/hooks/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getBearToken, getStoredAuthToken } from '../utils/auth.utils';
 
 const http = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://34.87.154.114.nip.io/gateway/api/',
 });
 // // In here, we handle the api error
 // http.interceptors.response.use(undefined, (error) =>

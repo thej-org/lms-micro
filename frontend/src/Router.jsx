f344c22b2853c81789383837d40713c7e109e932
pawan-live <pawan.senpura@gmail.com>
2025-05-08 01:26:03 +0530

    test:sonarFE

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -5,6 +5,7 @@ import { Admin, Home, Login } from './pages';
 import LeanerProfile from './pages/learner/LearnerProfile';
 
 const Router = () => {
+    
     const userRole = localStorage.getItem('userRole');
     const location = window.location;
     const navigate = useNavigate();
